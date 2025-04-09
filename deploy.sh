71a6f47824070ba67acbaac944fcce9ac49861c1
abhijeetpraveen <abhijeetpraveen@gmail.com>
2025-04-09 18:08:11 -0400

    files can be specified in script

--- deploy.sh
+++ deploy.sh
@@ -3,16 +3,25 @@
 # Check if commit message is provided
 if [ -z "$1" ]; then
     echo "Error: Please provide a commit message"
-    echo "Usage: ./deploy.sh \"Your commit message\""
+    echo "Usage: ./deploy.sh \"Your commit message\" [file1 file2 ...]"
     exit 1
 fi
 
 # Store the commit message
 commit_message="$1"
 
+# Shift to get the remaining arguments (files)
+shift
+
 # Git operations
 echo "Adding files to git..."
-git add .
+if [ $# -eq 0 ]; then
+    # If no files specified, add all changes
+    git add .
+else
+    # Add specified files
+    git add "$@"
+fi
 
 echo "Committing changes with message: $commit_message"
 git commit -m "$commit_message"
